af837dd49e8bbda98417dbf7f2edfa62177e4c43
xiaohoucc <1373916199@qq.com>
2022-03-02 16:09:50 +0800

    springboot练习

---
+++ src/main/java/org/chenchuang/helloWorldApplication.java
@@ -0,0 +1,11 @@
+package org.chenchuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class helloWorldApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(helloWorldApplication.class, args);
+    }
+}
